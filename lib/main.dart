43928838c2d3be9671bca9d044952d03c2e80502
John Doe <ok63477@gmail.com>
2024-11-27 07:44:10 +0900

    웹으로도 실행가능하게 설정

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,34 @@ import 'package:sharecalendar_app/sign_up_screen.dart';
 import 'calendar_page.dart';
 import 'login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'dart:html' as html; // 웹 관련 코드 처리
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   try {
-    await Firebase.initializeApp();
-    runApp(MyApp());
+    if (html.window.location.protocol.contains("http")) {
+      // 웹 플랫폼인 경우 Firebase 초기화
+      await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: "AIzaSyA3N-8q3SMRu981feqe_H1-UsPenuCf0SQ",
+            authDomain: "sharecalendar-8003f.firebaseapp.com",
+            databaseURL: "https://sharecalendar-8003f-default-rtdb.firebaseio.com",
+            projectId: "sharecalendar-8003f",
+            storageBucket: "sharecalendar-8003f.firebasestorage.app",
+            messagingSenderId: "629819734808",
+            appId: "1:629819734808:web:10ccb6cfbe7a4b2976fc6d",
+            measurementId: "G-4ZKV5N6C22"
+        ),
+      );
+    } else {
+      // 모바일 플랫폼인 경우
+      await Firebase.initializeApp();
+    }
+
   } catch (e) {
     print('Firebase 초기화 실패: $e');
   }
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
